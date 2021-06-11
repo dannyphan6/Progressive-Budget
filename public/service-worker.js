6a9dca66b19e42ef2bf73441504e3ce26a12789b
Daniel Phan <dannyphan@comcast.net>
2021-06-10 23:12:28 -0700

    Set up event listener for service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,19 @@ const FILES_TO_CACHE = [
     "index.html",
     "index.js",
     "styles.css",
+    "manifest.json",
     "./icons/icon-192x192.png",
     "./icons/icon-512x512.png"
 ]
+
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(PRECACHE).then((cache) => {
+            cache.addAll(FILES_TO_CACHE).then(self.skipWaiting())
+        })
+    )
+})
 
