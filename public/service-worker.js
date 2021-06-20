5466a97c07aa9633b74cf2e6ff77207d1e9c6133
Daniel Phan <dannyphan@comcast.net>
2021-06-19 22:39:44 -0700

    Refactored code

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,72 @@
 const FILES_TO_CACHE = [
-    "/",
-    "index.html",
-    "index.js",
-    "styles.css",
-    "manifest.json",
-    "./icons/icon-192x192.png",
-    "./icons/icon-512x512.png"
-]
+  "/",
+  "/index.html",
+  "/index.js",
+  "/styles.css",
+  "/manifest.json",
+  "/db.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
 
-const PRECACHE = "precache-v1";
-const RUNTIME = "runtime";
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
 
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open(PRECACHE).then((cache) => {
-            cache.addAll(FILES_TO_CACHE).then(self.skipWaiting())
-        })
-    )
-})
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
+});
 
+self.addEventListener('activate', (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(PRECACHE).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(err => {
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
+    );
+    return;
+  };
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
+      })
+    })
+  );
+});
